b070e85a7e1af95cff1d2a91feede6049212a28b
ToughDBlue <53195842+ToughDBlue@users.noreply.github.com>
2026-06-22 15:10:22 -0400

    Update example.sh

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -8,4 +8,4 @@ set -oue pipefail
 # Your code goes here.
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
-curl -fsSL https://raw.githubusercontent.com/yannmasoch/nautilus-my-computer/main/install.sh | sh
+# curl -fsSL https://raw.githubusercontent.com/yannmasoch/nautilus-my-computer/main/install.sh | sh
